ff3df65aa2a7210b711eb533e9029b4c086a1b53
ertoth <ertoth@paypal.com>
2013-12-19 18:41:23 -0800

    in it comm it

---
+++ test/shush.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var test = require('tape'),
+    shush = require('../');
+
+test('shush', function (t) {
+
+    function createSanity(file) {
+        return function sanity(t) {
+            var json = shush(file);
+            t.ok(json);
+            t.equal(typeof json, 'object');
+            t.deepEqual(json.foo, 'bar');
+            t.deepEqual(json.baz, 5);
+            t.deepEqual(json.bam, true);
+            t.end();
+        };
+    }
+
+    t.test('throws on missing file', function (t) {
+        t.throws(function () {
+            shush('./fixtures/nonexistent');
+        }, Error);
+        t.end();
+    });
+
+    t.test('Parse error should contain filename', function (t) {
+        var err = undefined;
+
+        try {
+            shush('./fixtures/invalid');
+        } catch (e) {
+            err = e;
+        } finally {
+            console.log(err);
+            t.ok(err instanceof SyntaxError);
+            t.ok(err.message.match(/fixtures\/invalid/));
+            t.ok(err);
+            t.end();
+        }
+    });
+
+    t.test('parses json', createSanity('./fixtures/json'));
+
+    t.test('removes line comments', createSanity('./fixtures/line'));
+
+    t.test('removes block comments', createSanity('./fixtures/block'));
+
+    t.test('removes both line and block comments', createSanity('./fixtures/combo'));
+
+});
